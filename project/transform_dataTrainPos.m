8dceb1c12d139ac76c58e42a6cc882c45a30651a
Markimk13 <31613461+Markimk13@users.noreply.github.com>
2019-03-29 00:31:25 +0100

    Save state

--- project/transform_dataTrainPos.m
+++ project/transform_dataTrainPos.m
@@ -4,31 +4,33 @@ function [img] = transform_dataTrainPos(oldImg, height, width, mode, resizeFacto
     img = oldImg;
     img = imresize(img, [size(img,1), size(img,2)]/resizeFactor);
     
+    % check if the image has the correct size
+    if strcmp(mode, 'plain') || strcmp(mode, 'locate')
+        if height ~= size(img, 1) || width ~= size(img, 2)
+            error("Image does not have the correct size.");
+        end
+    end
+    
     % set the img from gray to rgb
     if length(size(img)) == 2
         img = cat(3, img, img, img);
     end
     
     % nothing to do in this mode
-    if (strcmp(mode, 'plain'))
-        if height ~= size(img, 1) || width ~= size(img, 2)
-            error("Incorrect image size");
-        end
+    if strcmp(mode, 'plain')
         return;
     end
     
     % init the size for the image to return
     img_size = [height, width, 3];
     
-    % get the bounding box of the image
-    bb = get_bounding_box(img);
-    
     % reduce img to the bounding box
+    bb = get_bounding_box(img);
     img = img(bb(1)+(1:bb(3)),bb(2)+(1:bb(4)),:);
 
     if strcmp(mode, 'crop') || strcmp(mode, 'resize')
         % resize if the mode is 'resize' or the bb does not fit in the img_size
-        if strcmp(mode, 'resize') || bb(3) > height || bb(4) > width
+        if strcmp(mode, 'resize')
             img_height = min(height, bb(3)*width/bb(4));
             img_width = min(width, bb(4)*height/bb(3));
             img = imresize(img, [img_height, img_width]);
